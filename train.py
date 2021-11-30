72b9ea524d7cd1a7b9f0e3bc85ee5493019c038c
DgLee <tsi00008@gmail.com>
2021-11-30 21:30:20 +0900

    use argparse directly

--- train.py
+++ train.py
@@ -1,11 +1,9 @@
+import argparse
 import os
 import torch
-import numpy as np
 import pandas as pd
-from tqdm import tqdm
 from torch.utils.data import DataLoader
 
-from src.setup import set_env
 from src.dataset import OneHotEncoding, Dataset
 from src.model import MVAE
 from src.loss_function import LossFunction
@@ -15,48 +13,41 @@ from src.callback import EarlyStopping
 from src.trainer import Trainer
 
 
-def train_step(dataloader: DataLoader):
-    model.train()
-    loss_function.train()
-    losses = []
-
-    for batch in tqdm(dataloader):
-        optimizer.zero_grad()
-
-        inputs = batch["inputs"].to(device)
-        targets = batch["targets"].to(device)
-
-        outputs, mean, log_var = model(inputs)
-
-        loss = loss_function(outputs, targets, mean, log_var)
-        losses.append(loss.item())
-
-        loss.backward()
-        optimizer.step()
-
-    return np.mean(losses)
-
-
-def valid_step(dataloader: DataLoader):
-    model.eval()
-    loss_function.eval()
-    losses = []
-
-    with torch.no_grad():
-        for batch in tqdm(dataloader):
-            inputs = batch["inputs"].to(device)
-            targets = batch["targets"].to(device)
-
-            outputs = model(inputs)
-
-            loss = loss_function(outputs, targets)
-            losses.append(loss.item())
-
-    return np.mean(losses)
-
-
 if __name__ == "__main__":
-    args = set_env()
+    parser = argparse.ArgumentParser()
+
+    # model
+    parser.add_argument("--model-name", default="MVAE.pt", type=str)
+    parser.add_argument("--hidden-dim", default=256, type=int)
+    parser.add_argument("--n-layers", default=1, type=int)
+    parser.add_argument("--anneal-rate", default=0.3, type=float)
+    parser.add_argument("--seed", default=777, type=int, help="seed for random initialization")
+
+    # learning parameters
+    parser.add_argument("--n-epochs", default=10, type=int)
+    parser.add_argument("--batch-size", default=50, type=int)
+    parser.add_argument("--lr", default=0.05, type=float, help="learning rate")
+
+    # optimizer parameters
+    parser.add_argument("--optimizer-type", default="Adam", type=str, help="Adam, RMSProp, SGD")
+    parser.add_argument("--weight-decay", default=0.0, type=float)
+    parser.add_argument("--momentum", default=0.1, type=float)
+    parser.add_argument("--eps", default=1e-6, type=float)
+
+    # callback parameters
+    parser.add_argument("--patience", default=5, type=int, help="early stopping patience")
+    parser.add_argument("--delta", default=0.0, type=float, help="early stopping threshold")
+
+    # inference
+    parser.add_argument("--eval-k", default=25, type=int, help="how many items you recommend")
+    parser.add_argument("--user-id", type=int, help="user id")
+
+    # directions
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--checkpoint-dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
+    parser.add_argument("--data-dir", type=str, default=os.environ["SM_CHANNEL_TRAINING"])
+
+    args = parser.parse_args()
 
     torch.manual_seed(args.seed)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
